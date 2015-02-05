c7a6f72fa60f7ab872a9e92a8edbb52e1318556a
Bruno Bonnin <bbonnin@gmail.com>
2015-02-05 18:13:48 +0100

    Add HiveServer2 launcher

---
+++ src/main/java/hadoopmongo/HiveServer2Launcher.java
@@ -0,0 +1,23 @@
+package hadoopmongo;
+
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hive.service.server.HiveServer2;
+
+public class HiveServer2Launcher {
+
+    public static void main(String[] args) {
+        HiveConf hiveConf = new HiveConf();
+        hiveConf.set("javax.jdo.option.ConnectionURL", "jdbc:derby:;databaseName=metastore_db;create=true");
+        hiveConf.set("javax.jdo.option.ConnectionDriverName", "org.apache.derby.jdbc.EmbeddedDriver");
+        hiveConf.set("hive.metastore.warehouse.dir", "file:///tmp");
+
+        /*<!--hive.metastore.local=true
+                mapreduce.framework.name=yarn
+                hive.exec.submitviachild=false-->
+                hive.debug.localtask=true
+                hive.auto.convert.join.use.nonstaged=true*/
+        HiveServer2 server = new HiveServer2();
+        server.init(hiveConf);
+        server.start();
+    }
+}
